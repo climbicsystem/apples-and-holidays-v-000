07071826d9caa62fdb15c53e4d2bbe8aa187bac5
Joseph Vint <josephvint@me.com>
2018-02-18 17:39:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
+      binding.pry
       if supplies.include?("BBQ")
         bbq_holidays << holiday
       end
